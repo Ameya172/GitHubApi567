e970276e41a2a7809a765dbe6a80f7f27f013425
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-03-20 14:49:25 +0300

    Update Testhw4a.py

--- Testhw4a.py
+++ Testhw4a.py
@@ -3,7 +3,7 @@ import unittest
 class TestGetUserRepos(unittest.TestCase):
     def test_get_user_repos_success(self):
         user_id = "Ameya172"
-        expected_result = [("SSW567", 14)]
+        expected_result = [("SSW567", 20)]
         self.assertEqual(get_user_repos(user_id), expected_result)
 
     def test_get_user_repos_error(self):
